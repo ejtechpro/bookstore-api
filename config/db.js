5dd390501008f2c918169ad836822da32eadbbfe
EJ <ejtechpro@gmail.com>
2024-11-12 09:57:07 +0300

    Added mongo_uri

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const conn = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(
+      "mongodb+srv://ej:ej12644944@my-db.6kqqq.mongodb.net/?retryWrites=true&w=majority&appName=my-db"
+    );
     console.log("Connected to mongodb!");
   } catch (err) {
     console.log("Error: ", err);
