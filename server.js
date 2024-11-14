858d778bddac85c9c9541a23f366c8ba3516bee4
EJ <ejtechpro@gmail.com>
2024-11-14 16:35:36 +0300

    Initial commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const ejs = require("ejs");
+const cors = require("cors");
 const conn = require("./config/db");
 require("dotenv").config();
 
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 conn();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({origin: null} ));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join("public")));
