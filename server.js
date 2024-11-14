2362daa8cdc3ce8b499f2d731b1f4df753e3c4dd
EJ <ejtechpro@gmail.com>
2024-11-14 16:52:58 +0300

    Initial commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ conn();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({origin: null} ));
+app.use(cors());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join("public")));
